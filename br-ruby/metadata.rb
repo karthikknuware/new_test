a6fe027a77e987a11c287a50292ea1547d2944d7
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-04 16:31:26 +0530

    Added files via upload

---
+++ br-ruby/metadata.rb
@@ -0,0 +1,12 @@
+name              'br-ruby'
+version           '0.5.1'
+description       'Installs and manages Ruby versions'
+maintainer        'Jim Pruetting'
+maintainer_email  'core-team@builtbyrobots.com'
+license           'MIT License'
+source_url        'https://github.com/built-by-robots/br-ruby-cookbook'
+
+supports 'ubuntu'
+supports 'debian'
+supports 'centos'
+supports 'redhat'
